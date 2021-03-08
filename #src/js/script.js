f008e6e5a15589284f94d390144f4a9b21935f35
Eloy1337 <malsagov.01@mail.ru>
2021-03-09 01:17:01 +0300

    Завершил верстку

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,18 @@
-@@include("alert.js")
+// @@include("alert.js")
+let players = document.querySelectorAll(".player__container");
+let buttons = document.querySelectorAll(".player__buttons button");
+
+
+buttons[0].onclick = () => {
+	players[0].classList.remove("hidden");
+	players[1].classList.add("hidden");
+	buttons[0].classList.add("player__buttons--active");
+	buttons[1].classList.remove("player__buttons--active");
+}
+
+buttons[1].onclick = () => {
+	players[0].classList.add("hidden");
+	players[1].classList.remove("hidden");
+	buttons[0].classList.remove("player__buttons--active");
+	buttons[1].classList.add("player__buttons--active");
+}
